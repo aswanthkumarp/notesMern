a6e846d4b442b2cffa9098dfd6f2ae0b3c55612a
Aswanth Kumar P <aswanthkumar777@gmail.com>
2023-12-17 18:07:13 +0530

    last commit

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/mernEnable';
-
+require('dotenv').config();
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster69.smffsae.mongodb.net/?retryWrites=true&w=majority`
 const run = async () => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(uri);
     console.log('You have successfully connected to MongoDB!');
   } catch (error) {
     console.error(error);
